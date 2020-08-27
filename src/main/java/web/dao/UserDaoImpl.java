44355ef7f999dd18e9455993e45cfdde8f982fad
SDA-mind <dima-sherbakov@mail.ru>
2020-08-27 15:51:11 +0300

    last

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,7 +1,6 @@
 package web.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import web.model.User;
 
@@ -9,8 +8,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import java.util.List;
+
 @Repository
-@Component
 public class UserDaoImpl implements dao {
     @PersistenceContext
     EntityManager entityManager;
