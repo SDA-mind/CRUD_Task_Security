40c34fae03f9d2e794eb6c690c16a5604c88bb94
SDA-mind <dima-sherbakov@mail.ru>
2020-08-27 15:06:07 +0300

    last

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -3,62 +3,62 @@ package web.dao;
 import org.springframework.stereotype.Component;
 import web.model.User;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.transaction.Transactional;
 
+import java.util.List;
+@Transactional
 @Component
 public class UserDaoImpl implements dao {
-    private static Map<Integer, User> users = new HashMap<>();
-    static int i = 3;
-    static {
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("CRUD_task");
 
-        User user1 = new User();
-        user1.setId(1);
-        user1.setName("Bob");
-        user1.setLastName("White");
-        user1.setDate("15/02/2000");
-        users.put(user1.getId(), user1);
-
-        User user2 = new User();
-        user2.setId(2);
-        user2.setName("Sam");
-        user2.setLastName("Green");
-        user2.setDate("01/11/1980");
-        users.put(user2.getId(), user2);
-
-        User user3 = new User();
-        user3.setId(3);
-        user3.setName("Clare");
-        user3.setLastName("Jons");
-        user3.setDate("10/05/1990");
-        users.put(user3.getId(), user3);
-
-    }
     @Override
     public List<User> allUsers() {
-        return new ArrayList<>(users.values());
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        List<User> user = entityManager.createQuery("from User",User.class)
+                .getResultList();
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        return user;
     }
 
     @Override
     public void add(User user) {
-        user.setId(++i);
-        users.put(user.getId(),user);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(user);
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
 
     @Override
     public void delete(User user) {
-        users.remove(user.getId());
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.remove(entityManager.merge(user));
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
 
     @Override
     public void edit(User user) {
-        users.put(user.getId(),user);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.merge(user);
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
 
     @Override
     public User getById(int id) {
-        return users.get(id);
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        User user = entityManager.find(User.class,id);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        return user;
     }
 }
--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -2,11 +2,20 @@ package web.model;
 
 import org.springframework.stereotype.Component;
 
-@Component
+import javax.persistence.*;
+
+@Entity
+@Table
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "date")
     private String date;
 
     public User() {
