dbc2317a38e60486f975f4a588ca3bce400cfa8c
SDA-mind <dima-sherbakov@mail.ru>
2020-08-29 19:56:20 +0300

    last

--- src/main/java/web/dao/dao.java
+++ src/main/java/web/dao/dao.java
@@ -7,8 +7,9 @@ import java.util.List;
 @Component
 public interface dao {
     List<User> allUsers();
-    void add(User film);
-    void delete(User film);
-    void edit(User film);
+    void add(User user);
+    void delete(User user);
+    void edit(User user);
     User getById(int id);
+    User getByName(String name);
 }
